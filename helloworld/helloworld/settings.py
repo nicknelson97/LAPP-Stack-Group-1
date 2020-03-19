77bbc2a4ba8171d37d225603d0fec877ddd98b0f
Jenaleigh <59895752+Jenaleigh@users.noreply.github.com>
2020-03-19 13:28:41 -0700

    Update settings.py

--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hwdb',
-        'USER': 'postgres',
-        'PASSWORD': '509Cfimy2090',
+        'USER': 'hwapp',
+        'PASSWORD': 'P@ssw0rd1',
         'HOST': 'localhost',
         'PORT': '',
     }
